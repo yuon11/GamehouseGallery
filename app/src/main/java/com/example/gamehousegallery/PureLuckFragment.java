d6d72a54d817fa66c8871af3d45b28b9fd5fee16
yuon11 <yuonflemming@aol.com>
2021-12-20 10:48:47 -0500

    Finishing up game loop for Pureluck

--- app/src/main/java/com/example/gamehousegallery/PureLuckFragment.java
+++ app/src/main/java/com/example/gamehousegallery/PureLuckFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,6 +28,13 @@ public class PureLuckFragment extends Fragment {
     private Button toss_btn;
     private Button heads_btn;
     private Button tails_btn;
+    private Button log_score_btn;
+    private Button clear_btn;
+    private TextView toss_cnt_txtview;
+    private TextView score_txtview;
+    private TextView difficulty_txtview;
+    private TextView toss_txtview;
+
 
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
@@ -36,11 +44,18 @@ public class PureLuckFragment extends Fragment {
     FragmentManager parentFragManager;
     boolean mShowingBack;
 
-    private int toss_cnt;
+
+    private int heads_guess_cnt;
+    private int tails_guess_cnt;
     private int guess_cnt;
+
+    private int crrct_heads_guess_cnt;
+    private int crrct_tails_guess_cnt;
     private int crrct_guess_cnt;
+
+    private int toss_cnt;
     private String current_guess;
-    private boolean isGuessing;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,11 +65,12 @@ public class PureLuckFragment extends Fragment {
         currentUser = mAuth.getCurrentUser();
         parentFragManager = getParentFragmentManager();
 
-        toss_cnt=0;
-        guess_cnt=0;
-        crrct_guess_cnt=0;
-        current_guess = "";
-        isGuessing=false;
+        resetScoreData();
+
+
+        toss_cnt=0;     // num of times toss is selected in general
+        current_guess = ""; // if current_guess.equals.("")
+
 
         if (savedInstanceState == null) {
 
@@ -74,6 +90,7 @@ public class PureLuckFragment extends Fragment {
                     .commit();
         }
     }
+
 
 
     private void flipCard() {
@@ -138,21 +155,62 @@ public class PureLuckFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootview =inflater.inflate(R.layout.fragment_pureluck, container, false);
 
+
+//        TextView toss_cnt_txtview;
+//        TextView score_txtview;
+//        TextView toss_txtview;
+
+        // Text Views
+        toss_cnt_txtview = (TextView) rootview.findViewById(R.id.toss_cnt_textview);
+        score_txtview = (TextView) rootview.findViewById(R.id.score_textview);
+        difficulty_txtview = (TextView) rootview.findViewById(R.id.difficulty_textview);
+
+        resetGameUI();
+
         btn = (Button) rootview.findViewById(R.id.btn);
         toss_btn = (Button) rootview.findViewById(R.id.toss_btn);
         heads_btn= (Button) rootview.findViewById(R.id.heads);
         tails_btn= (Button) rootview.findViewById(R.id.tails);
+        log_score_btn= (Button) rootview.findViewById(R.id.log_score_btn);
+        clear_btn= (Button) rootview.findViewById(R.id.clear_btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(RANDOM.nextFloat() > 0.5f )
+                {
+                    Toast.makeText(getContext(), "Is THIS your lucky side?", Toast.LENGTH_SHORT).show();
+                }
+                else if(RANDOM.nextFloat() < 0.5f )
+                {
+                    Toast.makeText(getContext(), "OR, Is THIS your lucky side?", Toast.LENGTH_SHORT).show();
+                }
+
                 flipCard();
             }
         });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(RANDOM.nextFloat() > 0.5f )
+                {
+                    Toast.makeText(getContext(), "Is THIS your lucky side?", Toast.LENGTH_SHORT).show();
+                }
+                else if(RANDOM.nextFloat() < 0.5f )
+                {
+                    Toast.makeText(getContext(), "OR, Is THIS your lucky side?", Toast.LENGTH_SHORT).show();
+                }
+
+                flipCard();
+            }
+        });
+        // TOSS
         toss_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toss_cnt +=1;
+                // UPDATE GAME COUNTS
+                updateGameState();
 
                 new CountDownTimer(3000, 100) {
                     public void onTick(long millisUntilFinished) {
@@ -166,9 +224,11 @@ public class PureLuckFragment extends Fragment {
                         }else {
                             if(mShowingBack)
                                 flipCard();
+
                         }
-                        heads_btn.setBackgroundColor( getResources().getColor(R.color.duke_blue));
-                        tails_btn.setBackgroundColor( getResources().getColor(R.color.duke_blue));
+
+                        // Reset both buttons to blue
+                        updateGameUI();
                     }
                 }.start();
             }
@@ -205,9 +265,77 @@ public class PureLuckFragment extends Fragment {
                 }
             }
         });
-
-
         return rootview;
+    }
+
+    public void logScoreData(){
+
+    }
+
+    public void resetScoreData(){
+        heads_guess_cnt=0;
+        tails_guess_cnt=0;
+        guess_cnt=tails_guess_cnt +
+                heads_guess_cnt;    // num of times toss is selected WHILE either HEAD or TAILS is selected
+
+        crrct_heads_guess_cnt=0;
+        crrct_tails_guess_cnt=0;
+        crrct_guess_cnt=crrct_tails_guess_cnt +
+                crrct_heads_guess_cnt;  // num of TOTAL CORRECT GUESSES of heads plus tails
+    }
+
+    public void resetGameUI(){
+        String tossUpdate = "Toss"+ toss_cnt;
+        toss_cnt_txtview.setText(tossUpdate);
+
+        String scoreUpdate="Score:"+ crrct_guess_cnt + " / " + guess_cnt;
+        score_txtview.setText(scoreUpdate);
+
+        String difficultyUpdate ="Heads: "+heads_guess_cnt+"|"+crrct_heads_guess_cnt+"\n"
+                +"Tails: "+tails_guess_cnt+"|"+crrct_tails_guess_cnt;
+        difficulty_txtview.setText(difficultyUpdate);
+    }
+
+    public void updateGameState(){
+        // When user hits toss, what should we update
+        toss_cnt +=1;
+
+        if (current_guess.equals("heads"))
+        {
+            heads_guess_cnt+=1;
+            if (!mShowingBack){
+                Toast.makeText(getContext(), "CORRECT GUESS ON " + current_guess, Toast.LENGTH_SHORT).show();
+                crrct_heads_guess_cnt+=1;
+            }
+
+        }else if (current_guess.equals("tails"))
+        {
+            tails_guess_cnt+=1;
+            if (mShowingBack){
+                Toast.makeText(getContext(), "CORRECT GUESS ON " + current_guess, Toast.LENGTH_SHORT).show();
+                tails_guess_cnt+=1;
+            }
+        }
+        // reset current guess
+        guess_cnt=tails_guess_cnt +heads_guess_cnt;    // num of times toss is selected WHILE either HEAD or TAILS is selected
+        crrct_guess_cnt=crrct_tails_guess_cnt +crrct_heads_guess_cnt;
+        current_guess="";
+    }
+
+    public void updateGameUI(){
+        // When user hits toss, what should we update
+        heads_btn.setBackgroundColor( getResources().getColor(R.color.duke_blue));
+        tails_btn.setBackgroundColor( getResources().getColor(R.color.duke_blue));
+
+        String tossUpdate = "Toss: "+ toss_cnt;
+        toss_cnt_txtview.setText(tossUpdate);
+
+        String scoreUpdate="Score: "+ crrct_guess_cnt + " / " + guess_cnt;
+        score_txtview.setText(scoreUpdate);
+
+        String difficultyUpdate ="Heads: "+heads_guess_cnt+"|"+crrct_heads_guess_cnt+"\n"
+                +"Tails: "+tails_guess_cnt+"|"+crrct_tails_guess_cnt;
+        difficulty_txtview.setText(difficultyUpdate);
     }
 
     @Override
